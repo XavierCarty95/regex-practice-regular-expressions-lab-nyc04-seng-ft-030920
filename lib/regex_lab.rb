460e731c915bdbc979ed730496daf6ad2a96b8ad
Xavier Carty <xaviercarty@gmail.com>
2020-03-28 20:18:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+    word.match?(/[aeiou]/) && word.match?(/^[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/(^un)(ing)/)
 end
 
 def words_five_letters_long(text)
-
+     text.match?(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match?(/^[A-Z]$\w/)
 
 end
 
 def valid_phone_number?(phone)
+  phone.scan(/(\d+)-(\d+)-(\d+)/)
 
 end
